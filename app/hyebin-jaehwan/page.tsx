bdf4731d5af07bbbe36d201b3e6b21060de00532
wlsud801 <wlsud801@gmail.com>
2026-04-16 09:57:06 +0900

    빌드 오류 수정

--- app/hyebin-jaehwan/page.tsx
+++ app/hyebin-jaehwan/page.tsx
@@ -1,16 +1,16 @@
 export const dynamic = "force-dynamic";
 
-import IntroSection from "./components/sections/IntroSection";
-import RSVPButton from "./components/RSVPButton";
-import InvitationSection from "./components/sections/InvitationSection";
-import FamilySection from "./components/sections/FamilySection";
-import CalendarSection from "./components/sections/CalendarSection";
-import GallerySection from "./components/sections/GallerySection";
-import NoticeSection from "./components/sections/NoticeSection";
-import MessageSection from "./components/sections/MessageSection";
-import LocationSection from "./components/sections/LocationSection";
-import GiftSection from "./components/sections/GiftSection";
-import ThanksSection from "./components/sections/ThanksSection";
+import IntroSection from "@/app/components/sections/IntroSection";
+import RSVPButton from "@/app/components/RSVPButton";
+import InvitationSection from "@/app/components/sections/InvitationSection";
+import FamilySection from "@/app/components/sections/FamilySection";
+import CalendarSection from "@/app/components/sections/CalendarSection";
+import GallerySection from "@/app/components/sections/GallerySection";
+import NoticeSection from "@/app/components/sections/NoticeSection";
+import MessageSection from "@/app/components/sections/MessageSection";
+import LocationSection from "@/app/components/sections/LocationSection";
+import GiftSection from "@/app/components/sections/GiftSection";
+import ThanksSection from "@/app/components/sections/ThanksSection";
 
 export default function Page() {
   return (
